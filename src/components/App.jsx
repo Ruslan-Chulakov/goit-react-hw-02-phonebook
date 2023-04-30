8bfd4fd210b27943bf22490b4458caea12a92765
Ruslan-Chulakov <AN_124_100@ukr.net>
2023-04-30 20:18:48 +0300

    1ST

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,78 @@
 import { Component } from 'react';
-import Contacts from './Contacts';
-import Phonebook from './Phonebook';
-
+import { Notify } from 'notiflix';
+import clsx from 'clsx'
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import css from './App.module.css'
 
 export class App extends Component {
   state = {
-    contacts: ["Harry Potter", 'Germiona Granger', 'Ron Whisley'],
-    name: '',
+    contacts: [
+      { id: 'klajsdf', name: 'Harry Potter', number: '111-11-11' },
+      { id: 'faslkj', name: 'Hermiona Grainger', number: '222-22-22' },
+      { id: 'asdf', name: 'Ron Whisley', number: '333-33-33' },
+      { id: 'wiow', name: 'Dart Vaider', number: '777-77-77' },
+    ],
+    filter: '',
   };
 
-  render() {
+  handleAddContact = contact => {
+    if (this.state.contacts.find(person => person.name === contact.name)) {
+      Notify.failure('This name is already exist');
+      return;
+    }
+    if (this.state.contacts.find(person => person.number === contact.number)) {
+      Notify.failure('This number is already exist');
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
 
-    const {contacts} = this.state
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleChange = evt => this.setState({ filter: evt.target.value });
+
+  handleFilterContacts = () =>
+    this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase().trim())
+    );
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = this.handleFilterContacts();
 
     return (
-      <div>
-        <Phonebook />
-        <Contacts contacts={contacts} />
+      <div className={clsx(css.container)}>
+        <span className={clsx(css.titleSpan)}>
+          <h1 className={clsx(css.title)}>Phonebook</h1>
+        </span>
+        <ContactForm handleAddContact={this.handleAddContact} />
+
+        {contacts.length !== 0 && <span className={clsx(css.titleSpan)}><h2 className={clsx(css.title)}>Contacts</h2></span>}
+
+        {contacts.length > 3 && (
+          <Filter value={filter} handleChange={this.handleChange} />
+        )}
+
+        {contacts.length !== 0 && (
+          <ContactList
+            contacts={filteredContacts}
+            handleDelete={this.handleDelete}
+          />
+        )}
       </div>
     );
   }
-};
+}
+
+
+// styled components
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,29 +1,29 @@
-function Phonebook() {
-  return (
-    <form>
-      <h2>Phonebook </h2>
-      <label htmlFor="name">Name</label>
-          <input
-              placeholder="Harry Potter"
-        type="text"
-        name="name"
-        id="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-      <label htmlFor="tel">Telephone</label>
-          <input
-              placeholder="380979379992"
-        type="tel"
-        name="tel"
-        id="tel"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-      <button type="button">Add contact</button>
-    </form>
-  );
-}
+// function Phonebook() {
+//   return (
+//     <form>
+//       <h2>Phonebook </h2>
+//       <label htmlFor="name">Name</label>
+//           <input
+//               placeholder="Harry Potter"
+//         type="text"
+//         name="name"
+//         id="name"
+//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//         required
+//       />
+//       <label htmlFor="tel">Telephone</label>
+//           <input
+//               placeholder="380979379992"
+//         type="tel"
+//         name="tel"
+//         id="tel"
+//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//         required
+//       />
+//       <button type="button">Add contact</button>
+//     </form>
+//   );
+// }
 
-export default Phonebook;
+// export default Phonebook;
